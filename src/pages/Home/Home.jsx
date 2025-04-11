dc03a9c2d68f0d112c96922ed3a6bec9cd19e2ee
buwanikaveesha <buwanikaveesha@gmail.com>
2025-04-11 11:58:00 +0530

    css update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Hero from "../../components/Hero/Hero";
 import HowToDo from "../HowToDo/HowToDo";
 import FAQ from "../FAQ/FAQ";
+import Features from "../Features/Features";
 
 
 const Home = () => {
   return (
     <div className="main_page-container">
       <Hero />
-      <HowToDo />
+      <Features />
+      {/* <HowToDo /> */}
       <FAQ/>
     </div>
   );
